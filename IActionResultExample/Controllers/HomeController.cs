e8ac67fc1f1da32114270c12ab92cb9919a62315
TheLukCraft <luk1997r@o2.pl>
2023-10-26 12:55:12 +0200

    Status code results

--- IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/Controllers/HomeController.cs
@@ -10,32 +10,30 @@ namespace IActionResultExample.Controllers
             //Book id should be applied
             if (!Request.Query.ContainsKey("bookid"))
             {
-                Response.StatusCode = 400;
-                return Content("Book id is not supplied");
+                //return new BadRequestResult();
+                return BadRequest("Book id is not supplied");
             }
             //Book id can't be empty
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
             {
-                Response.StatusCode = 400;
-                return Content("Book id can't be null or empty");
+                return BadRequest("Book id can't be null or empty");
             }
 
             //Book id should be between 1 to 1000
             int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookid"]);
             if (bookId <= 0)
             {
-                Response.StatusCode = 400;
-                return Content("Book id cant be less than 0 equal to zero.");
+                return BadRequest("Book id cant be less than 0 equal to zero.");
             }
             if (bookId > 1000)
             {
-                Response.StatusCode = 400;
-                return Content("Book id can't be greater than 1000.");
+                return NotFound("Book id can't be greater than 1000.");
             }
             //isLoggedIn should be true
             if (!Convert.ToBoolean(Request.Query["isloggedin"]))
             {
-                return Content("User must be authenticated");
+                //return Unauthorized("User must be authenticated");
+                return StatusCode(401);
             }
             return File("/photo.jpg", "image/jpg");
         }
