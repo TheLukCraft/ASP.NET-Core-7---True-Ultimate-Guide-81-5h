e2627eed0fb4a5ced54e9a691e2c308fe9e92ea4
TheLukCraft <luk1997r@o2.pl>
2023-10-26 08:52:39 +0200

    Creating controllers

---
+++ ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController
+    {
+        [Route("sayhello")]
+        public string method1()
+        {
+            return "Hello from method1";
+        }
+    }
+}
--- ControllersExample/Program.cs
+++ ControllersExample/Program.cs
@@ -1,6 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using ControllersExample.Controllers;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+//Instead of adding one controller at a time via AddTransient, then you can use the AddControllers method and then it will add all of them.
+//builder.Services.AddTransient<HomeController>();
+builder.Services.AddControllers();
+var app = builder.Build();
+app.UseRouting();
+//adds all controllers that implement Controller. You do not need to manually make endpoints here
+app.MapControllers();
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();
+//});
 
 app.Run();
