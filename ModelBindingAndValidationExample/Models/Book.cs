8a949637b8830f6688cf6abfce1f9f78704a8e2a
TheLukCraft <luk1997r@o2.pl>
2023-10-26 21:43:04 +0200

    Model class - in progress

---
+++ ModelBindingAndValidationExample/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelBindingAndValidationExample.Models
+{
+    public class Book
+    {
+        public int? BookId { get; set; }
+        public string? Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book id {BookId}, Author: {Author}";
+        }
+    }
+}
