b2efcfad501c5ea11dff07ae6bebb7bf427c561c
TheLukCraft <luk1997r@o2.pl>
2023-10-27 11:23:34 +0200

    Model state

--- ModelValidationsExample/Controllers/HomeController.cs
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -8,6 +8,22 @@ namespace ModelValidationsExample.Controllers
         [Route("register")]
         public IActionResult Index(Person person)
         {
+            if (!ModelState.IsValid)
+            {
+                List<string> errorsList = new List<string>();
+                ////First way - longer way without LINQ
+                //foreach (var value in ModelState.Values)
+                //{
+                //    foreach (var error in value.Errors)
+                //    {
+                //        errorsList.Add(error.ErrorMessage);
+                //    }
+                //}
+                string errors = string.Join("\n",ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+                //string errors = string.Join("\n", errorsList);
+                return BadRequest(errors);
+            }
+
             return Content($"{person}");
         }
     }
--- ModelValidationsExample/Models/Person.cs
+++ ModelValidationsExample/Models/Person.cs
@@ -1,8 +1,12 @@
-﻿namespace ModelValidationsExample.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationsExample.Models
 {
     public class Person
     {
+        [Required(ErrorMessage = "Person name can't be empty or null")]
         public string? PersonName { get; set; }
+
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Password { get; set; }
