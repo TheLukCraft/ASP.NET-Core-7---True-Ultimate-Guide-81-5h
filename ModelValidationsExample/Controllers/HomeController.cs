c7440aa90efb3fe117ad031d46676d56d02dbc7c
TheLukCraft <luk1997r@o2.pl>
2023-10-27 18:07:48 +0200

    From body

--- ModelValidationsExample/Controllers/HomeController.cs
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -6,7 +6,8 @@ namespace ModelValidationsExample.Controllers
     public class HomeController : Controller
     {
         [Route("register")]
-        public IActionResult Index([Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Password), nameof(Person.ConfirmPassword))] Person person)
+        //public IActionResult Index([Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Password), nameof(Person.ConfirmPassword))] Person person)
+        public IActionResult Index([FromBody] Person person)
         {
             if (!ModelState.IsValid)
             {
