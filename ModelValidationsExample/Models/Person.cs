64f30f219b5d390e4703cadc6b5873a268343467
TheLukCraft <luk1997r@o2.pl>
2023-10-27 09:34:06 +0200

    Introduction to model validations

---
+++ ModelValidationsExample/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace ModelValidationsExample.Models
+{
+    public class Person
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object - Person name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, ConfirmPassword: {ConfirmPassword}, Price: {Price} ";
+        }
+    }
+}
